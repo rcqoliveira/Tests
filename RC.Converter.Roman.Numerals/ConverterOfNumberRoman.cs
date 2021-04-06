58c2e3c5b0fd65be621453ea6995cf093c4090f6
rcqol <rcqol@DESKTOP-RMT1CKD>
2021-04-06 19:01:55 -0300

    #Add project

---
+++ RC.Converter.Roman.Numerals/ConverterOfNumberRoman.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RC.Converter.Roman.Numerals
+{
+    public class ConverterOfNumberRoman
+    {
+        private readonly Dictionary<string, int> tableOfNumberRoman = new Dictionary<string, int>()
+        {
+            { "I", 1 },
+            { "V", 5 },
+            { "X", 10 },
+            { "L", 50 },
+            { "C", 100 },
+            { "D", 500 },
+            { "M", 1000 }
+        };
+
+        public int ToConvert(string numberInRoman)
+        {
+            int acumulator = 0;
+            int lastNumberOnTheRight = 0;
+
+            for (int i = numberInRoman.Length - 1; i >= 0; i--)
+            {
+                int current = tableOfNumberRoman[numberInRoman[i].ToString()];
+                int multiplier = 1;
+
+                if (current < lastNumberOnTheRight) multiplier = -1;
+                acumulator += current * multiplier;
+                lastNumberOnTheRight = current;
+            }
+
+            return acumulator;
+        }
+    }
+}
